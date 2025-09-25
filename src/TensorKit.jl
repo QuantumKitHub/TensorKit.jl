3237aa71676bf33250dd9495dca5f09f7eda15fe
Boris De Vos <boris.devos@ugent.be>
2025-09-25 16:08:26 +0200

    export new functions from TensorKitSectors

--- src/TensorKit.jl
+++ src/TensorKit.jl
@@ -10,14 +10,17 @@ module TensorKit
 # Types:
 export Sector, AbstractIrrep, Irrep
 export FusionStyle, UniqueFusion, MultipleFusion, MultiplicityFreeFusion,
-    SimpleFusion, GenericFusion
+       SimpleFusion, GenericFusion
+export UnitStyle, SimpleUnit, GenericUnit
 export BraidingStyle, SymmetricBraiding, Bosonic, Fermionic, Anyonic, NoBraiding
 export Trivial, Z2Irrep, Z3Irrep, Z4Irrep, ZNIrrep, U1Irrep, SU2Irrep, CU1Irrep
 export ProductSector
 export FermionParity, FermionNumber, FermionSpin
 export FibonacciAnyon, IsingAnyon
+export unit, rightunit, leftunit, allunits, isunit
 
 export VectorSpace, Field, ElementarySpace # abstract vector spaces
+export unitspace
 export InnerProductStyle, NoInnerProduct, HasInnerProduct, EuclideanInnerProduct
 export ComplexSpace, CartesianSpace, GeneralSpace, GradedSpace # concrete spaces
 export ZNSpace, Z2Space, Z3Space, Z4Space, U1Space, CU1Space, SU2Space
