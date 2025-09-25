71a6ba2cf7b3b94dcfbeb0c2148544a05e8ca931
Lukas Devos <ldevos98@gmail.com>
2025-09-25 17:34:29 -0400

    remove more unused files

--- src/auxiliary/linalg.jl
+++
@@ -1,4 +0,0 @@
-# Simple reference to getting and setting BLAS threads
-#------------------------------------------------------
-set_num_blas_threads(n::Integer) = LinearAlgebra.BLAS.set_num_threads(n)
-get_num_blas_threads() = LinearAlgebra.BLAS.get_num_threads()
