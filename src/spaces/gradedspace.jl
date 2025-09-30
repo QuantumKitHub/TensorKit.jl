a0e474faf938c09faa9acc4a4cf602ca5d7f0c1e
Boris De Vos <boris.devos@ugent.be>
2025-09-30 11:44:23 +0200

    have `dim` of graded space depend on sectorscalartype

--- src/spaces/gradedspace.jl
+++ src/spaces/gradedspace.jl
@@ -91,8 +91,9 @@ Base.hash(V::GradedSpace, h::UInt) = hash(V.dual, hash(V.dims, h))
 field(::Type{<:GradedSpace}) = ℂ
 InnerProductStyle(::Type{<:GradedSpace}) = EuclideanInnerProduct()
 function dim(V::GradedSpace)
+    T = Base.promote_op(*, Int, real(sectorscalartype(sectortype(V))))
     return reduce(+, dim(V, c) * dim(c) for c in sectors(V);
-                  init=zero(dim(unit(sectortype(V)))))
+                  init=zero(T))
 end
 function dim(V::GradedSpace{I,<:AbstractDict}, c::I) where {I<:Sector}
     return get(V.dims, isdual(V) ? dual(c) : c, 0)
