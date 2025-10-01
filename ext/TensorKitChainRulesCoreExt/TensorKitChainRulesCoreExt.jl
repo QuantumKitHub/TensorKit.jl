39f20b1a30ae8c49e6eb6995e1b93d9d98c4b18a
Lukas Devos <ldevos98@gmail.com>
2025-10-01 17:32:03 -0400

    remove factorization chainrules

--- ext/TensorKitChainRulesCoreExt/TensorKitChainRulesCoreExt.jl
+++ ext/TensorKitChainRulesCoreExt/TensorKitChainRulesCoreExt.jl
@@ -12,16 +12,9 @@ import TensorOperations as TO
 using TensorOperations: promote_contract, tensoralloc_add, tensoralloc_contract
 using VectorInterface: promote_scale, promote_add
 
-using MatrixAlgebraKit
-using MatrixAlgebraKit: TruncationStrategy, TruncatedAlgorithm,
-                        svd_pullback!, eig_pullback!, eigh_pullback!,
-                        qr_compact_pullback!, lq_compact_pullback!,
-                        left_polar_pullback!, right_polar_pullback!
-
 include("utility.jl")
 include("constructors.jl")
 include("linalg.jl")
 include("tensoroperations.jl")
-include("factorizations.jl")
 
 end
