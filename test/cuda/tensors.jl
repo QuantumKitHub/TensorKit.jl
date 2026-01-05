7fc983a787ff7018672f998e21bcc5f47493efe0
Lukas Devos <ldevos98@gmail.com>
2026-01-05 23:07:00 +0100

    add Adapt tests

--- test/cuda/tensors.jl
+++ test/cuda/tensors.jl
@@ -115,6 +115,18 @@ for V in spacelist
                 @test domain(t2) == one(W)
             end
         end
+        @timedtestset "Adapt" begin
+            W = V1 ⊗ V2 ⊗ V3 ⊗ V4 ⊗ V5
+            t = rand(Float64, W)
+            t_gpu = @constinferred adapt(CuArray, t)
+            @test storagetype(t_gpu) <: CuArray
+            @test scalartype(t_gpu) === scalartype(t)
+            @test collect(t_gpu.data) == t.data
+
+            t_cpu = @constinferred adapt(Array, t_cpu)
+            @test t_cpu == t
+            @test storagetype(t_cpu) isa Array
+        end
         @timedtestset "Tensor Dict conversion" begin
             W = V1 ⊗ V2 ⊗ V3 ← V4 ⊗ V5
             for T in (Int, Float32, ComplexF64)
