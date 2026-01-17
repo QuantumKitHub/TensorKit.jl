545835d1957074f3f87fb32e0a1c3a8e2e522756
Lukas Devos <ldevos98@gmail.com>
2026-01-17 09:03:23 +0100

    add `arrayify` for adjoint tensor

--- ext/TensorKitMooncakeExt/tangent.jl
+++ ext/TensorKitMooncakeExt/tangent.jl
@@ -5,3 +5,11 @@ function Mooncake.arrayify(A_dA::CoDual{<:TensorMap})
     dA = typeof(A)(data, A.space)
     return A, dA
 end
+
+function Mooncake.arrayify(Aᴴ_ΔAᴴ::CoDual{<:TensorKit.AdjointTensorMap})
+    Aᴴ = Mooncake.primal(Aᴴ_ΔAᴴ)
+    ΔAᴴ = Mooncake.tangent(Aᴴ_ΔAᴴ)
+    A_ΔA = CoDual(Aᴴ', ΔAᴴ.data.parent)
+    A, ΔA = arrayify(A_ΔA)
+    return A', ΔA'
+end
