3f273a1e20cba238015d572d3b0097c388dbda68
lkdvos <ldevos98@gmail.com>
2026-01-22 16:55:31 -0500

    remove CUDA specializations and temporarily add missing MatrixAlgebraKit thingies

--- ext/TensorKitCUDAExt/truncation.jl
+++ ext/TensorKitCUDAExt/truncation.jl
@@ -1,20 +1,7 @@
-const CuSectorVector{T, I} = TensorKit.SectorVector{T, I, <: CuVector{T}}
-
-function Factorizations._findtruncvalue_order(
-    values::CuSectorVector, n::Int; by = identity, rev::Bool = false
-)
-    I = sectortype(values)
-    p = sortperm(parent(values); by, rev)
-
-    if FusionStyle(I) isa UniqueFusion # dimensions are all 1
-        return n <= 0 ? nothing : @allowscalar(by(values[p[min(n, length(p))]]))
-    else
-        dims = similar(values, Base.promote_op(dim, I))
-        for (c, v) in pairs(dims)
-            fill!(v, dim(c))
-        end
-        cumulative_dim = cumsum(Base.permute!(parent(dims), p))
-        k = findlast(<=(n), cumulative_dim)
-        return isnothing(k) ? k : @allowscalar(by(values[p[k]]))
-    end
+function MatrixAlgebraKit._ind_intersect(A::AbstractVector{Bool}, B::CuVector{Int})
+    return MatrixAlgebraKit._ind_intersect(findall(A), B)
 end
+
+# TODO: intersect doesn't work on GPU
+MatrixAlgebraKit._ind_intersect(A::CuVector{Int}, B::CuVector{Int}) =
+    MatrixAlgebraKit._ind_intersect(collect(A), collect(B))
