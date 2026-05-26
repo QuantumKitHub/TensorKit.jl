27df7f1fcc9fd851e6738c421a7a2fd2e8493c3d
Katharine Hyatt <katharine.s.hyatt@gmail.com>
2026-05-26 12:10:21 +0200

    Fix utility and avoid const-A in forward mode

--- ext/TensorKitEnzymeExt/utility.jl
+++ ext/TensorKitEnzymeExt/utility.jl
@@ -30,9 +30,9 @@ project_scalar(x::Real, dx::Complex) = project_scalar(x, real(dx))
 
 @inline EnzymeRules.inactive(::typeof(TensorKit.sectorstructure), ::Any) = nothing
 @inline EnzymeRules.inactive(::typeof(TensorKit.degeneracystructure), ::Any) = nothing
-@inline EnzymeRules.inactive(::typeof(TensorKit.select), s::HomSpace, i::Index2Tuple) = nothing
+@inline EnzymeRules.inactive(::typeof(TensorKit.select), s::HomSpace, i::TensorKit.Index2Tuple) = nothing
 @inline EnzymeRules.inactive(::typeof(TensorKit.flip), s::HomSpace, i::Any) = nothing
-@inline EnzymeRules.inactive(::typeof(TensorKit.permute), s::HomSpace, i::Index2Tuple) = nothing
-@inline EnzymeRules.inactive(::typeof(TensorKit.braid), s::HomSpace, i::Index2Tuple, ::IndexTuple) = nothing
+@inline EnzymeRules.inactive(::typeof(TensorKit.permute), s::HomSpace, i::TensorKit.Index2Tuple) = nothing
+@inline EnzymeRules.inactive(::typeof(TensorKit.braid), s::HomSpace, i::TensorKit.Index2Tuple, ::TensorKit.IndexTuple) = nothing
 @inline EnzymeRules.inactive(::typeof(TensorKit.compose), s1::HomSpace, s2::HomSpace) = nothing
-@inline EnzymeRules.inactive(::typeof(TensorOperations.tensorcontract), c::HomSpace, p::Index2Tuple, α::Bool, b::HomSpace, q::Index2Tuple, β::Bool, pq::Index2Tuple) = nothing
+#@inline EnzymeRules.inactive(::typeof(TensorOperations.tensorcontract), c::HomSpace, p::TensorKit.Index2Tuple, α::Bool, b::HomSpace, q::TensorKit.Index2Tuple, β::Bool, pq::TensorKit.Index2Tuple) = nothing
