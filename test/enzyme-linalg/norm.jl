5357ee2614bcd082813f556b49d52cc0aee6d0f3
Katharine Hyatt <katharine.s.hyatt@gmail.com>
2026-06-11 09:17:50 +0200

    Fix tensor for norm

--- test/enzyme-linalg/norm.jl
+++ test/enzyme-linalg/norm.jl
@@ -14,7 +14,7 @@ fRTs = is_ci ? (Duplicated,) : (Const, Duplicated)
     @timedtestset "$(TensorKit.type_repr(sectortype(eltype(V)))) ($T), TC $TC" for V in spacelist, T in eltypes, TC in (Const, Duplicated)
         atol = default_tol(T)
         rtol = default_tol(T)
-        C = randn(T, V[1] ⊗ V[2] ← V[5])
+        C = randn(T, V[1] ⊗ V[2] ← (V[3] ⊗ V[4] ⊗ V[5])')
         for RT in rRTs
             EnzymeTestUtils.test_reverse(norm, RT, (C, TC), (2, Const); atol, rtol)
             EnzymeTestUtils.test_reverse(norm, RT, (C', TC), (2, Const); atol, rtol)
