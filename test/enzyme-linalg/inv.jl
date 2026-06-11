178109326ff882efc0f8521ec0b347cdfe935ff6
Katharine Hyatt <katharine.s.hyatt@gmail.com>
2026-06-11 08:12:57 +0200

    Try to cut down on ci times

--- test/enzyme-linalg/inv.jl
+++ test/enzyme-linalg/inv.jl
@@ -6,6 +6,9 @@ using Random
 spacelist = ad_spacelist(fast_tests)
 eltypes = (Float64, ComplexF64)
 
+is_ci = get(ENV, "CI", "false") == "true"
+TDs = is_ci ? (Duplicated,) : (Const, Duplicated)
+
 @timedtestset "Enzyme - LinearAlgebra (inv):" begin
     @timedtestset "$(TensorKit.type_repr(sectortype(eltype(V)))) ($T)" for V in spacelist, T in eltypes
         atol = default_tol(T)
@@ -13,7 +16,7 @@ eltypes = (Float64, ComplexF64)
         D1 = randn(T, V[1] ← V[1])
         D2 = randn(T, V[1] ⊗ V[2] ← V[1] ⊗ V[2])
         D3 = randn(T, V[1] ⊗ V[2] ⊗ V[3] ← V[1] ⊗ V[2] ⊗ V[3])
-        @testset "inv: TD $TD" for TD in (Const, Duplicated)
+        @testset "inv: TD $TD" for TD in TDs 
             EnzymeTestUtils.test_reverse(inv, TD, (D1, TD); atol, rtol)
             EnzymeTestUtils.test_reverse(inv, TD, (D2, TD); atol, rtol)
             EnzymeTestUtils.test_reverse(inv, TD, (D3, TD); atol, rtol)
--- test/enzyme-linalg/norm.jl
+++ test/enzyme-linalg/norm.jl
@@ -6,16 +6,20 @@ using Random
 spacelist = ad_spacelist(fast_tests)
 eltypes = (Float64, ComplexF64)
 
+is_ci = get(ENV, "CI", "false") == "true"
+rRTs = is_ci ? (Active,) : (Const, Active)
+fRTs = is_ci ? (Duplicated,) : (Const, Duplicated)
+
 @timedtestset verbose = true "Enzyme - LinearAlgebra (norm):" begin
     @timedtestset "$(TensorKit.type_repr(sectortype(eltype(V)))) ($T), TC $TC" for V in spacelist, T in eltypes, TC in (Const, Duplicated)
         atol = default_tol(T)
         rtol = default_tol(T)
         C = randn(T, V[1] ⊗ V[2] ← V[5])
-        for RT in (Const, Active)
+        for RT in rRTs 
             EnzymeTestUtils.test_reverse(norm, RT, (C, TC), (2, Const); atol, rtol)
             EnzymeTestUtils.test_reverse(norm, RT, (C', TC), (2, Const); atol, rtol)
         end
-        for RT in (Const, Duplicated)
+        for RT in fRTs
             EnzymeTestUtils.test_forward(norm, RT, (C, TC), (2, Const); atol, rtol)
             EnzymeTestUtils.test_forward(norm, RT, (C', TC), (2, Const); atol, rtol)
         end
