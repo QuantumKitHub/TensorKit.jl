2554d8d62d340bf421b37ae88825a1b03cafe41e
Katharine Hyatt <katharine.s.hyatt@gmail.com>
2026-06-11 08:12:57 +0200

    Try to cut down on ci times

--- test/enzyme-linalg/mul.jl
+++ test/enzyme-linalg/mul.jl
@@ -6,6 +6,10 @@ using Random
 spacelist = ad_spacelist(fast_tests)
 eltypes = (Float64, ComplexF64)
 
+is_ci = get(ENV, "CI", "false") == "true"
+rTs = is_ci ? (Active,) : (Const, Active)
+fTs = is_ci ? (Duplicated,) : (Const, Duplicated)
+
 @timedtestset verbose = true "Enzyme - LinearAlgebra (mul):" begin
     @timedtestset "$(TensorKit.type_repr(sectortype(eltype(V)))) ($T)" for V in spacelist, T in eltypes
         atol = default_tol(T)
@@ -17,10 +21,10 @@ eltypes = (Float64, ComplexF64)
         α = randn(T)
         β = randn(T)
         @testset "mul: TC $TC, TA $TA, TB $TB" for TC in (Duplicated,), TA in (Duplicated,), TB in (Duplicated,)
-            @testset "Tα $Tα, Tβ $Tβ" for Tα in (Active, Const), Tβ in (Active, Const)
+            @testset "Tα $Tα, Tβ $Tβ" for Tα in rTs, Tβ in rTs
                 EnzymeTestUtils.test_reverse(mul!, TC, (C, TC), (A, TA), (B, TB), (α, Tα), (β, Tβ); atol, rtol)
             end
-            @testset "Tα $Tα, Tβ $Tβ" for Tα in (Duplicated, Const), Tβ in (Duplicated, Const)
+            @testset "Tα $Tα, Tβ $Tβ" for Tα in fTs, Tβ in fTs
                 EnzymeTestUtils.test_forward(mul!, TC, (C, TC), (A, TA), (B, TB), (α, Tα), (β, Tβ); atol, rtol)
             end
             EnzymeTestUtils.test_reverse(mul!, TC, (C, TC), (A, TA), (B, TB); atol, rtol)
