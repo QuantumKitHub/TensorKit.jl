b905c2e1c1e6428180b5ad2ff008aaae2fb13a30
Katharine Hyatt <kshyatt@users.noreply.github.com>
2026-06-25 19:16:10 -0400

    Forward AD tests and cleanup for vector interface (#436)
    * Forward rules for vector interface
    
    * Drop VI versions that don't have AD rules

--- ext/TensorKitMooncakeExt/utility.jl
+++ ext/TensorKitMooncakeExt/utility.jl
@@ -23,6 +23,7 @@ Mooncake.tangent_type(::Type{<:HomSpace}) = Mooncake.NoTangent
 @zero_derivative DefaultCtx Tuple{typeof(TensorKit.has_shared_permute), AbstractTensorMap, Index2Tuple}
 @zero_derivative DefaultCtx Tuple{typeof(TensorKit.select), HomSpace, Index2Tuple}
 @zero_derivative DefaultCtx Tuple{typeof(TensorKit.flip), HomSpace, Any}
+@zero_derivative DefaultCtx Tuple{typeof(TensorKit.adjoint), HomSpace}
 @zero_derivative DefaultCtx Tuple{typeof(TensorKit.permute), HomSpace, Index2Tuple}
 @zero_derivative DefaultCtx Tuple{typeof(TensorKit.braid), HomSpace, Index2Tuple, IndexTuple}
 @zero_derivative DefaultCtx Tuple{typeof(TensorKit.compose), HomSpace, HomSpace}
