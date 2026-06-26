bf8c3af6a3c254d21dbd599456c3d730668b00fc
sanderdemeyer <sander.demeyer@ugent.be>
2026-06-26 18:44:06 +0200

    Fix formatting

--- test/tensors/exponential.jl
+++ test/tensors/exponential.jl
@@ -2,24 +2,24 @@ using Test, TestExtras
 using TensorKit
 using Random
 
-spaces = [ℂ^4, Vect[U1Irrep](0 => 1, 1 => 2), Vect[SU2Irrep](0 => 1, 1//2 => 1)]
+spaces = [ℂ^4, Vect[U1Irrep](0 => 1, 1 => 2), Vect[SU2Irrep](0 => 1, 1 // 2 => 1)]
 scalartypes = [Float64, ComplexF64]
 
-@timedtestset "exp!(τ,A) for $space, scalartype(A) = $st1, scalartype(τ) = $st2" for space = spaces, st1 = scalartypes, st2 = scalartypes
+@timedtestset "exp!(τ,A) for $space, scalartype(A) = $st1, scalartype(τ) = $st2" for space in spaces, st1 in scalartypes, st2 in scalartypes
     A = randn(st1, space, space)
     τ = rand(st2)
 
     @test exp!(copy(A)) == exp!((1.0, copy(A)))
 
-    A2 = exp!((τ,A))
+    A2 = exp!((τ, A))
     if st1 <: Real && st2 <: Complex
         @test objectid(A2) != objectid(A)
     else
         @test objectid(A2) == objectid(A)
     end
 
-    expτA = exp!((τ,copy(A)))
-    expminτA = exp!((-τ,copy(A)))
+    expτA = exp!((τ, copy(A)))
+    expminτA = exp!((-τ, copy(A)))
     @test expτA * expminτA ≈ id(scalartype(expτA), space)
     @test expτA ≈ inv(expminτA)
 end
