ef5d7febf1b1fa4e2f34c30bc36aec3c5e1c8f9d
Katharine Hyatt <khyatt@flatironinstitute.org>
2026-06-27 10:30:02 -0400

    Cleanup and soothe permute

--- test/enzyme-indexmanipulations-transform/permute.jl
+++ test/enzyme-indexmanipulations-transform/permute.jl
@@ -17,18 +17,13 @@ Tβs = is_ci ? (Active,) : (Active, Const)
         rtol = default_tol(T)
         symmetricbraiding = BraidingStyle(sectortype(eltype(V))) isa SymmetricBraiding
 
-        symmetricbraiding && @timedtestset "permute!" begin
-            # repeat a couple times to get some distribution of arrows
-            for ri in 1:5
-                @testset for Tα in Tαs, Tβ in Tβs
-                    A = randn(T, V[1] ⊗ V[2] ← V[4] ⊗ V[5])
-                    α = randn(T)
-                    β = randn(T)
-                    p = randindextuple(numind(A))
-                    C = randn!(permute(A, p))
-                    EnzymeTestUtils.test_reverse(TensorKit.permute!, Duplicated, (C, Duplicated), (A, Duplicated), (p, Const), (α, Tα), (β, Tβ); atol, rtol)
-                end
-            end
+        symmetricbraiding && @timedtestset "permute! Tα $Tα, Tβ $Tβ" for Tα in Tαs, Tβ in Tβs
+            A = randn(T, V[1] ⊗ V[2] ← V[4] ⊗ V[5])
+            α = randn(T)
+            β = randn(T)
+            p = randindextuple(numind(A))
+            C = randn!(permute(A, p))
+            EnzymeTestUtils.test_reverse(TensorKit.permute!, Duplicated, (C, Duplicated), (A, Duplicated), (p, Const), (α, Tα), (β, Tβ); atol, rtol)
         end
     end
 end
