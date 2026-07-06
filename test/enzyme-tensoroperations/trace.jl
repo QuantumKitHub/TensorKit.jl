7432ac57f96ad49a6c319d54b8fa660fafd329f4
Katharine Hyatt <khyatt@flatironinstitute.org>
2026-07-06 13:51:34 -0400

    Don't run on Windows 1.10 for now

--- test/enzyme-tensoroperations/trace.jl
+++ test/enzyme-tensoroperations/trace.jl
@@ -15,44 +15,46 @@ fTβs = is_ci ? (Duplicated,) : (Const, Duplicated)
 TCs = is_ci ? (Duplicated,) : (Const, Duplicated)
 TAs = is_ci ? (Duplicated,) : (Const, Duplicated)
 
-@timedtestset "Enzyme - TensorOperations (trace)" begin
-    @timedtestset verbose = true "$(TensorKit.type_repr(sectortype(eltype(V)))) ($T)" for V in spacelist, T in eltypes
-        atol = default_tol(T)
-        rtol = default_tol(T)
-        symmetricbraiding = BraidingStyle(sectortype(eltype(V))) isa SymmetricBraiding
-        symmetricbraiding && @timedtestset "trace_permute!" begin
-            k1 = rand(0:2)
-            k2 = rand(1:2)
-            V1 = map(v -> rand(Bool) ? v' : v, rand(V, k1))
-            V2 = map(v -> rand(Bool) ? v' : v, rand(V, k2))
+if !(is_ci && Sys.iswindows() && VERSION < v"1.11.0-rc")
+    @timedtestset "Enzyme - TensorOperations (trace)" begin
+        @timedtestset verbose = true "$(TensorKit.type_repr(sectortype(eltype(V)))) ($T)" for V in spacelist, T in eltypes
+            atol = default_tol(T)
+            rtol = default_tol(T)
+            symmetricbraiding = BraidingStyle(sectortype(eltype(V))) isa SymmetricBraiding
+            symmetricbraiding && @timedtestset "trace_permute!" begin
+                k1 = rand(0:2)
+                k2 = rand(1:2)
+                V1 = map(v -> rand(Bool) ? v' : v, rand(V, k1))
+                V2 = map(v -> rand(Bool) ? v' : v, rand(V, k2))
 
-            (_p, _q) = randindextuple(k1 + 2 * k2, k1)
-            p = _repartition(_p, rand(0:k1))
-            q = _repartition(_q, k2)
-            ip = _repartition(invperm(linearize((_p, _q))), rand(0:(k1 + 2 * k2)))
-            A = randn(T, permute(prod(V1) ⊗ prod(V2) ← prod(V2), ip))
+                (_p, _q) = randindextuple(k1 + 2 * k2, k1)
+                p = _repartition(_p, rand(0:k1))
+                q = _repartition(_q, k2)
+                ip = _repartition(invperm(linearize((_p, _q))), rand(0:(k1 + 2 * k2)))
+                A = randn(T, permute(prod(V1) ⊗ prod(V2) ← prod(V2), ip))
 
-            α = randn(T)
-            β = randn(T)
-            C = randn!(TensorOperations.tensoralloc_add(T, A, p, false, Val(false)))
-            for TC in TCs, TA in TAs
-                for Tα in rTαs, Tβ in rTβs
-                    EnzymeTestUtils.test_reverse(
-                        TensorKit.trace_permute!, TC,
-                        (copy(C), TC), (A, TA), (p, Const), (q, Const),
-                        (α, Tα), (β, Tβ), (TensorOperations.DefaultBackend(), Const);
-                        atol, rtol,
-                        testset_name = "trace_permute! reverse TC $TC TA $TA Tα $Tα Tβ $Tβ",
-                    )
-                end
-                for Tα in fTαs, Tβ in fTβs
-                    EnzymeTestUtils.test_forward(
-                        TensorKit.trace_permute!, TC,
-                        (copy(C), TC), (A, TA), (p, Const), (q, Const),
-                        (α, Tα), (β, Tβ), (TensorOperations.DefaultBackend(), Const);
-                        atol, rtol,
-                        testset_name = "trace_permute! forward TC $TC TA $TA Tα $Tα Tβ $Tβ",
-                    )
+                α = randn(T)
+                β = randn(T)
+                C = randn!(TensorOperations.tensoralloc_add(T, A, p, false, Val(false)))
+                for TC in TCs, TA in TAs
+                    for Tα in rTαs, Tβ in rTβs
+                        EnzymeTestUtils.test_reverse(
+                            TensorKit.trace_permute!, TC,
+                            (copy(C), TC), (A, TA), (p, Const), (q, Const),
+                            (α, Tα), (β, Tβ), (TensorOperations.DefaultBackend(), Const);
+                            atol, rtol,
+                            testset_name = "trace_permute! reverse TC $TC TA $TA Tα $Tα Tβ $Tβ",
+                        )
+                    end
+                    for Tα in fTαs, Tβ in fTβs
+                        EnzymeTestUtils.test_forward(
+                            TensorKit.trace_permute!, TC,
+                            (copy(C), TC), (A, TA), (p, Const), (q, Const),
+                            (α, Tα), (β, Tβ), (TensorOperations.DefaultBackend(), Const);
+                            atol, rtol,
+                            testset_name = "trace_permute! forward TC $TC TA $TA Tα $Tα Tβ $Tβ",
+                        )
+                    end
                 end
             end
         end
