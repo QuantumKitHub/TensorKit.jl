92a405ebe08140b624a8d0147aeec59fef1ab867
Katharine Hyatt <kslimes@gmail.com>
2026-07-31 11:38:19 +0200

    Try combining flip and twist

--- test/enzyme-indexmanipulations-flip-twist-transform/flip.jl
+++ test/enzyme-indexmanipulations-flip-twist-transform/flip.jl
@@ -8,13 +8,27 @@ eltypes = (Float64, ComplexF64)
 
 is_ci = get(ENV, "CI", "false") == "true"
 
-@timedtestset "Enzyme - Index Manipulations (flip):" begin
+@timedtestset "Enzyme - Index Manipulations (flip and twist):" begin
     @timedtestset "$(TensorKit.type_repr(sectortype(eltype(V)))) ($T) TA ($TA)" for V in spacelist, T in eltypes, TA in (Duplicated,)
         atol = default_tol(T)
         rtol = default_tol(T)
         has_braiding = BraidingStyle(sectortype(eltype(V))) isa HasBraiding
         if has_braiding
             A = randn(T, V[1] ⊗ V[2] ← (V[3] ⊗ V[4] ⊗ V[5])')
+            if !(T <: Real && !(sectorscalartype(sectortype(A)) <: Real))
+                EnzymeTestUtils.test_reverse(twist!, TA, (A, TA), (1, Const); atol, rtol, fkwargs = (inv = false,))
+                EnzymeTestUtils.test_reverse(twist!, TA, (A, TA), ([1, 3], Const); atol, rtol, fkwargs = (inv = true,))
+                if !is_ci
+                    EnzymeTestUtils.test_reverse(twist!, TA, (A, TA), (1, Const); atol, rtol)
+                    EnzymeTestUtils.test_reverse(twist!, TA, (A, TA), ([1, 3], Const); atol, rtol)
+                end
+                EnzymeTestUtils.test_forward(twist!, TA, (A, TA), (1, Const); atol, rtol, fkwargs = (inv = false,))
+                EnzymeTestUtils.test_forward(twist!, TA, (A, TA), ([1, 3], Const); atol, rtol, fkwargs = (inv = true,))
+                if !is_ci
+                    EnzymeTestUtils.test_forward(twist!, TA, (A, TA), (1, Const); atol, rtol)
+                    EnzymeTestUtils.test_forward(twist!, TA, (A, TA), ([1, 3], Const); atol, rtol)
+                end
+            end
             EnzymeTestUtils.test_reverse(flip, TA, (A, TA), (1, Const); atol, rtol, fkwargs = (inv = false,))
             EnzymeTestUtils.test_reverse(flip, TA, (A, TA), ([1, 3], Const); atol, rtol, fkwargs = (inv = true,))
             if !is_ci
